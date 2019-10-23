44bd327b166ebaf9c6b16da2a5648d6a6d9fee59
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-10-23 15:16:16 +0100

    Add description to euler.rb

--- euler.rb
+++ euler.rb
@@ -1,6 +1,13 @@
-1000.times do |i|
-    if i%3==0 || i%5==0
-        s+= i
-        puts s
-    end
+# Create a procedure that "puts" the sum of the integers below 1000 that are divisible by 3 or 5
+
+# Solution 1
+sum = 0
+1000.times do |num|
+  if num % 3 == 0 || num % 5 == 0
+    sum += num
+  end
 end
+puts sum
+
+# Solution 2
+puts 1000.times.select { |num| num % 3 == 0 || num % 5 == 0 } .sum
