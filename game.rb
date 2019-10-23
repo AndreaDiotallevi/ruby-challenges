ca066f48dbf1064aa06567a391c0b8ff449c40bd
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-10-23 15:33:42 +0100

    Add description to game.rb

--- game.rb
+++ game.rb
@@ -1,22 +1,21 @@
-# Prompt the user the game has started and that he is faced forward
+# The user starts by facing forward. They can move right, left, or forward.
+# If they move right, they die (there's a goblin).
+# If they move left, they die (there's a werewolf).
+# If they move forward, they live. They have to do this twice to win.
+
 puts "The game has started and you are faced foward."
-# Prompt the user the commands he must enter to go right, left or forward
 puts "Type 'r' to move right, 'l' to move left and 'f' to move forward."
-# Create variable "score" that stores the amount of times the user has gone forward
 score = 0
 
 while true do
   user_choice = gets.chomp
   case user_choice
-  # If the user move right he dies becuase there is a goblin
   when 'r'
     puts "You die! A goblin just killed you!"
     break
-  # If the user move left he dies because there is a werewolf
   when 'l'
     puts "You die! A werewolf just killed you!"
     break
-  # If the user move forward increase score by 1. If score equals 2 display a winning message.
   when 'f'
     score += 1
     if score == 2
